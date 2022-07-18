b50318a110a02c461325158a9773871a22969334
seonyeong nam <iop1104@hanmail.net>
2022-07-18 13:41:13 +0900

    Create Project

---
+++ src/test/java/com/ezen/joinTest.java
@@ -0,0 +1,17 @@
+package com.ezen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class joinTest {
+
+	@Test
+	public joinTest() {
+		System.out.println("feature/1 브랜치 테스트 코드 ");
+		System.out.println("issue#1");
+	}
+
+}
